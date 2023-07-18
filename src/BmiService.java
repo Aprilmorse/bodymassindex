650fdfedd11cff8586711a5b9a874790ab326962
79911 <Petrovlistov@gmail.com>
2023-07-18 23:44:49 +0300

    Commit message

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+class BmiService { private final double weight;
+    private final double height;
+    public BmiService(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    } public double calculate() {
+        return weight / (Math.pow(height, 2));
+    }
+}
