d25966fd8090830303838d3e00e9ded57f3caed3
79911 <Petrovlistov@gmail.com>
2023-07-19 17:25:50 +0300

    Revert "Commit message"
    This reverts commit 650fdfedd11cff8586711a5b9a874790ab326962.

--- src/BmiService.java
+++
@@ -1,9 +0,0 @@
-class BmiService { private final double weight;
-    private final double height;
-    public BmiService(double weight, double height) {
-        this.weight = weight;
-        this.height = height;
-    } public double calculate() {
-        return weight / (Math.pow(height, 2));
-    }
-}
--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BmiService bmiService = new BmiService(98, 1.87);
-        int result = (int) bmiService.calculate();
-        System.out.println("Индекс массы тела:" + result);
-    }
-}
